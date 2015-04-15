ef4c55fe92201f76d36ac7c1acac203e5d35b5de
Col BT <KernalBT@gmail.com>
2015-04-15 00:43:05 -0400

    Initial System

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.0.0.92/v2/lcl/');
+define('HTTP_CATALOG', 'http://10.0.0.92/v2/');
+define('HTTP_HELP', 'http://www.TroyContainerLine.com/wiki/index.php/WebApplication/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.0.0.92/v2/lcl/');
+define('HTTPS_CATALOG', 'http://10.0.0.92/v2/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/troy/Troyv2/lcl/');
+define('DIR_SYSTEM', '/usr/share/troy/Troyv2/system/');
+define('DIR_DATABASE', '/usr/share/troy/Troyv2/system/database/');
+define('DIR_LANGUAGE', '/usr/share/troy/Troyv2/lcl/language/');
+define('DIR_TEMPLATE', '/usr/share/troy/Troyv2/lcl/view/template/');
+define('DIR_CONFIG', '/usr/share/troy/Troyv2/system/config/');
+define('DIR_IMAGE', '/usr/share/troy/Troyv2/image/');
+define('DIR_CACHE', '/usr/share/troy/Troyv2/system/cache/');
+define('DIR_DOWNLOAD', '/usr/share/troy/Troyv2/download/');
+define('DIR_LOGS', '/usr/share/troy/Troyv2/system/logs/');
+define('DIR_CATALOG', '/usr/share/troy/Troyv2/catalog/');
+define('DIR_UPLOAD', '/usr/share/troy/Troyv2/system/upload/');
+define('DIR_MODIFICATION', '/usr/share/troy/Troyv2/system/modification/');
+define('DIR_REPORTS', '/usr/share/troy/Troyv2/reports/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'troycontainerline.com');
+define('DB_USERNAME', 'troyweb1');
+define('DB_PASSWORD', 'Troyweb1');
+define('DB_DATABASE', 'troyv2');
+define('DB_PREFIX', 'troy_');
+define('DB_PREFIX_APP', 'lcl_');
+
